f104abb34cc24b6381860b9e3b402e79f3a07c68
Guilherme Lanza <s2it_glanza@uolinc.com>
2020-02-14 16:39:55 -0300

    14/fev

--- src/test/java/br/com/lanza/runners/Runner.java
+++ src/test/java/br/com/lanza/runners/Runner.java
@@ -1,14 +1,15 @@
 package br.com.lanza.runners;
 
+import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
+import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
-import io.cucumber.junit.CucumberOptions;
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions.SnippetType;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/resources/features/aprender_cucumber.feature",
-        glue = "br.com.lanza.steps",
+        features = "src/test/resources/features/aprender_cucumber.feature", //caminho das feature
+        glue = "br.com.lanza.steps", //caminho dos steps
+        //tags = "test", //Executa só esse cenário
         plugin = "pretty",
         dryRun = false,
         strict = true,
--- src/test/java/br/com/lanza/steps/AprenderCucumberSteps.java
+++ src/test/java/br/com/lanza/steps/AprenderCucumberSteps.java
@@ -1,8 +1,10 @@
 package br.com.lanza.steps;
 
-import io.cucumber.java.pt.Dado;
-import io.cucumber.java.pt.Então;
-import io.cucumber.java.pt.Quando;
+import br.com.lanza.converters.DateConverter;
+import cucumber.api.Transform;
+import cucumber.api.java.pt.Dado;
+import cucumber.api.java.pt.Então;
+import cucumber.api.java.pt.Quando;
 import org.junit.Assert;
 
 import java.text.DateFormat;
@@ -10,38 +12,35 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
-public class AprenderCucumber {
+public class AprenderCucumberSteps {
 
     private int contador = 0;
 
-
-    @Dado("que o valor do contador é {int}")
+    @Dado("que o valor do contador é (\\d+)$")
     public void queOValorDoContadorÉ(Integer int1) {
         contador = int1;
     }
 
-    @Quando("eu incrementar em {int}")
+    @Quando("eu incrementar em (\\d+)$")
     public void euIncrementarEm(Integer int1) {
         contador = contador + int1;
     }
 
-    @Então("o valor será {int}")
+    @Então("o valor será (\\d+)$")
     public void oValorSerá(Integer int1) {
         //Assert.assertTrue(int1 == contador);
         Assert.assertEquals(new Double(int1), new Double(contador));
     }
 
     Date entrega = new Date();
-    @Dado("que a entrega é dia {int}\\/{int}\\/{int}")
-    public void queAEntregaÉDia(Integer dia, Integer mes, Integer ano) {
-        Calendar cal = Calendar.getInstance();
-        cal.set(Calendar.DAY_OF_MONTH, dia);
-        cal.set(Calendar.MONTH, mes-1);
-        cal.set(Calendar.YEAR, ano);
-        entrega = cal.getTime();
+
+    @Dado("que a entrega é dia (.*)$")
+    public void queAEntregaÉDia(@Transform(DateConverter.class) Date data) throws Throwable {
+        System.out.println("---------------" + data);
+        entrega = data;
     }
 
-    @Quando("a entrega atrasar em {int} dias")
+    @Quando("a entrega atrasar em (\\d+) dias$")
     public void aEntregaAtrasarEmDias(Integer int1) {
         Calendar cal = Calendar.getInstance();
         cal.setTime(entrega);
@@ -49,8 +48,8 @@ public class AprenderCucumber {
         entrega = cal.getTime();
     }
 
-    @Então("a entrega será efetuada em (\\d{2}\\/\\d{2}\\/\\d{4})$")
-    public void aEntregaSeráEfetuadaEm(String data) {
+    @Então("^a entrega será efetuada em (\\d{2}\\/\\d{2}\\/\\d{4})$")
+    public void aEntregaSeráEfetuadaEm(String data) throws Throwable {
         DateFormat format = new SimpleDateFormat("dd/MM/yyyy");
         String dataFormatada = format.format(entrega);
         Assert.assertEquals(data, dataFormatada);
